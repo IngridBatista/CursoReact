933557bf7a329540774925414b98d1a80a323292
Ingrid Batista <fingridbatista@gmail.com>
2021-04-04 12:58:39 -0300

    Repetição

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,7 @@
-import './App.css'
+import "./App.css";
 import React from "react";
 
+import Lista from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Card from "./components/layout/Card";
@@ -8,12 +9,17 @@ import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 
 export default (_) => (
   <div className="App">
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
+      <Card titulo="#06 - Repetição" color="##FF4C65">
+        <ListaAlunos></ListaAlunos>
+      </Card>
+
       <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
         <Familia sobrenome="Silva">
           <FamiliaMembro nome="Pedro" />
